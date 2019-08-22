bf262319ed3ae2ba2172606fc10a4e42ab4d3f3f
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-08-22 14:59:38 +0530

    add file Main.java

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ * @author Hardik Gupta
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        System.out.print("Welcome, " + s);
+
+
+    }
+}
