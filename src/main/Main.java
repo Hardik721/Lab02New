8ea91403f1cb9f81ce85dd4c3547c7ed3d2b3628
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-08-22 15:13:42 +0530

    scanner class ka ek object banaya hai

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,18 @@
 package main;
 
 
+import java.util.Scanner;
+
 /**
  * @author Hardik Gupta
  * @version 1.0
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String username = "";
+        scanner.close();
+
 
     }
 
