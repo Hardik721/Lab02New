23cb29c53e638614396a26bc166476368610c73e
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-08-22 15:32:26 +0530

    final output

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,6 +12,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String username = scanner.nextLine().trim();
         System.out.println("Welcome, " + username + "!");
+        System.out.println("done");
         scanner.close();
 
 
