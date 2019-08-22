5d062ce388ee995202a464195be31401828b055f
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-08-22 15:10:44 +0530

    variable declared for input

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,13 @@
 package main;
 
 
-
 /**
  * @author Hardik Gupta
  * @version 1.0
  */
 public class Main {
     public static void main(String[] args) {
+        String username = "";
 
     }
 
